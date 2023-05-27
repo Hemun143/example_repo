4c717bcce37d611ada541d2cf679827cfb52d470
Hemun143 <hemun14syno@gmail.com>
2023-05-27 18:05:02 +0000

    second commit

---
+++ code1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+name='Himanshu Sharma'
+echo "Hi Developer $name !!"
+sur_name='Mr. Sharma'
+status=prefection
+focus=peak
+exp='focous and potential'
+show="Your $status will reach in $focus with $exp !!"
+echo "Developer $sur_name $show"
